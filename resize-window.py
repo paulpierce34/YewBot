821fa8cb67fcc838b61117351e24a23b84fe98ea
paulpierce34 <33561650+paulpierce34@users.noreply.github.com>
2023-02-21 22:22:54 -0500

    Add files via upload

---
+++ resize-window.py
@@ -0,0 +1,26 @@
+import pyautogui
+import time
+ 
+# Set the desired position and size of the window
+x = 100
+y = 100
+width = 1000
+height = 850
+ 
+# Find the window with the title "RuneLite"
+window_title = "Old School RuneScape"
+window = pyautogui.getWindowsWithTitle(window_title)[0]
+if window is None:
+    print(f"Error: Could not find window with title {window_title}")
+else:
+    # Bring the window to the foreground
+    window.activate()
+ 
+    # Wait a short time for the window to become active
+    time.sleep(0.5)
+ 
+    # Move the window to the specified location and set the size
+    window.resizeTo(width, height)
+    window.moveTo(x, y)
+ 
+    print("Window moved successfully!")
